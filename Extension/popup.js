b8ad430964364e527ccec2625e76475051703b6c
Eslam Khaled <eslaamkhaled64@gmail.com>
2023-10-11 01:07:25 +0300

    Force text-only view as default

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,7 +1,7 @@
 document.getElementById('readMedium').addEventListener('click', function() {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       const mediumUrl = tabs[0].url;
-      const googleCacheUrl = `http://webcache.googleusercontent.com/search?q=cache:${mediumUrl}`;
+      const googleCacheUrl = `http://webcache.googleusercontent.com/search?q=cache:${mediumUrl}&sca_esv=572350337&strip=1&vwsrc=0`;
       chrome.tabs.create({ url: googleCacheUrl });
     });
   });
